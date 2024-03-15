a8a4a2e25689fd02022dd52cdedcad26dd15aff3
Shaibmen <ibin2005636@gmail.com>
2024-03-15 17:42:50 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _1PractPractika.DataSet1TableAdapters;
+
+
+namespace _1PractPractika
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ChurchTableAdapter church = new ChurchTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            FirstGrid.ItemsSource = church.GetData();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new Page3();
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new Page2();
+        }
+    }
+}
---
+++ Page2.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _1PractPractika.DataSet1TableAdapters;
+
+namespace _1PractPractika
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        PriestTableAdapter priest = new PriestTableAdapter();
+        public Page2()
+        {
+            InitializeComponent();
+            SecondGrid.ItemsSource = priest.GetData();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new MainWindow();
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Content = new Page3();
+        }
+    }
+}
